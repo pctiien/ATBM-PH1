5fece6e250d164aa0f984ddc99a754925a5a24a4
96leohan <129765801+96leohan@users.noreply.github.com>
2025-04-26 05:07:32 +0700

    Update IOracleRepository.cs

--- Repository/IOracleRepository.cs
+++ Repository/IOracleRepository.cs
@@ -13,5 +13,25 @@ namespace ATBM_HTTT_PH1.Repository
         Task<List<string[]>> getPermissionByRole(string roleName);
         Task<List<string>> getRoles();
         Task<List<string[]>> getUsers();
+
+        Task<bool> CreateUser(string username, string password, bool isDBA = false);
+        Task<bool> DropUser(string username, bool cascade = false);
+        Task<bool> AlterUser(string username, string newPassword = null, bool? locked = null);
+        
+    
+        Task<bool> CreateRole(string roleName, string password = null);
+        Task<bool> DropRole(string roleName);
+        Task<bool> AlterRole(string roleName, string newPassword = null);
+        
+      
+        Task<bool> GrantPrivilegeToUser(string privilege, string objectName, string username, bool withGrantOption = false);
+        Task<bool> GrantPrivilegeToRole(string privilege, string objectName, string roleName);
+        Task<bool> GrantRoleToUser(string roleName, string username);
+        Task<bool> RevokePrivilegeFromUser(string privilege, string objectName, string username);
+        Task<bool> RevokePrivilegeFromRole(string privilege, string objectName, string roleName);
+        Task<bool> RevokeRoleFromUser(string roleName, string username);
+        
+       
+        Task<DataTable> ExecuteQuery(string sql);
     }
 }
